950e54076ef245cc24875629e9bbf296bb00f202
mdtalha109 <mdtalha1041@gmail.com>
2022-10-05 18:19:56 +0530

    added logout features

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -10,17 +10,29 @@ export class HeaderComponent implements OnInit {
 
   constructor(private route : Router) { }
   menuType :String = 'default'
+  sellerName : String = ''
   ngOnInit(): void {
     this.route.events.subscribe((val:any)=> {
       if(val.url){
         console.log(val.url)
         if(localStorage.getItem('seller') && val.url.includes('seller')){
           this.menuType = 'seller'
+          if(localStorage.getItem('seller')){
+            let sellerDetails = localStorage.getItem('seller');
+            let sellerData = sellerDetails && JSON.parse(sellerDetails)[0];
+            this.sellerName = sellerData.name
+          }
         }else{
           this.menuType = 'default'
         }
       }
     })
+  }
+
+
+  logout(){
+    localStorage.removeItem('seller');
+    this.route.navigate(['/'])
   }
 
 }
