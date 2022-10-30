9102aaa84d8da8415a5b974e99c77d4192ee7705
mdtalha109 <mdtalha1041@gmail.com>
2022-10-30 19:57:29 +0530

    implemented search suggestion

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { product } from '../data-type';
+import { ProductService } from '../services/product.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  popularProduct : undefined | product[]
+  trendyProducts: undefined | product[]
 
-  constructor() { }
+  constructor(private product: ProductService) { }
+  images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`); 
 
   ngOnInit(): void {
+    this.product.popularProduct().subscribe((result) => {
+      this.popularProduct = result
+    })
+
+    this.product.trendyProducts().subscribe((data) => {
+      this.trendyProducts = data
+    })
   }
 
 }
